0ce27a52a1ee3b43456f6226a4fe6a7314f88e7a
jarhells <jarhells@gmail.com>
2016-05-16 21:08:04 +0300

    Test server

---
+++ client_jquery/testServer/server.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.use(express.static('../app'));
+
+var theWord = 'SALASANA';
+
+var server = app.listen(9000, function () 
+{
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+})
+
+app.get('/HirsiServer_JAX-RS/services/words/getWord', function (req, res)
+{
+    res.json( { id: '5', length: theWord.length } );
+})
+
+app.get('/HirsiServer_JAX-RS/services/words/letterInWord/:id', function (req, res)
+{
+    var id = req.params.id;
+    var letter = req.query.letter;
+
+    var jsonData = {};
+
+    for( var i = 0; i < theWord.length; i++ )
+    {
+        if( theWord[i] == letter )
+        {
+            jsonData[i] = letter;  
+        }
+    }
+
+    res.json( jsonData );
+})
